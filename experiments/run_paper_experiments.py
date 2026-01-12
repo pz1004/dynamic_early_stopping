66b82d2e6399ac216b8e66c52533084ff74c65e3
Sooyoung Jang <pz1004@gmail.com>
2026-01-12 23:30:55 +0900

    a

--- experiments/run_paper_experiments.py
+++ experiments/run_paper_experiments.py
@@ -63,7 +63,7 @@ DES_KNN_PCA_TAUS = {
     'synthetic_uniform': [2, 5, 10, 20, 50, 100, 200],
     # sift1m: All taus achieve 0.9994 recall
     'sift1m': [100, 200, 400, 800, 1600, 3200, 6400],
-    'glove': [5, 6, 7, 8, 9, 10],
+    'glove': [3, 4, 5, 6, 7],
 }
 
 DES_KNN_GUARANTEE_TAUS = {
@@ -71,10 +71,11 @@ DES_KNN_GUARANTEE_TAUS = {
     'mnist': [10, 20, 30, 40, 50],
     'fashion_mnist': [1, 5, 10, 20, 50, 100, 200],
     # synthetic_clustered/uniform: 0.99 at tau~20
-    'synthetic_clustered': [2, 5, 10, 20, 50, 100, 200],
+    'synthetic_clustered': [5, 15, 20, 25, 30],
     'synthetic_uniform': [2, 5, 10, 20, 50, 100, 200],
     # sift1m: Does NOT achieve 0.99 (max ~0.4), include for completeness
     'sift1m': [1, 5, 10, 50, 100, 500, 1000],
+    # done
     'glove': [5, 10, 15, 20, 25],
 }
 
@@ -83,12 +84,12 @@ HNSW_EF_SWEEPS = {
     'mnist': [8, 16, 32, 64, 128],
     'fashion_mnist': [8, 16, 32, 64, 128],
     # synthetic_clustered: 0.99 at ef~100
-    'synthetic_clustered': [20, 50, 100, 200, 400, 800],
+    'synthetic_clustered': [50, 100, 150, 200, 250],
     # synthetic_uniform: Never reaches 0.99 (max 0.93 at ef=800)
     'synthetic_uniform': [100, 200, 400, 800, 1600, 3200],
     # sift1m: 0.99 at ef~200
     'sift1m': [50, 100, 200, 400, 600, 800, 1200],
-    'glove': [50, 100, 200, 400, 600, 800, 1200],
+    'glove': [500, 600, 700, 800, 900, 1000],
 }
 
 ANNOY_SEARCHK_SWEEPS = {
@@ -96,12 +97,12 @@ ANNOY_SEARCHK_SWEEPS = {
     'mnist': [2500, 5000, 7500, 10000, 12500],
     'fashion_mnist': [1000, 2000, 5000, 10000, 20000, 40000],
     # synthetic_clustered: 0.99 at search_k~5000
-    'synthetic_clustered': [1000, 2000, 5000, 10000, 20000, 40000],
+    'synthetic_clustered': [2500, 5000, 7500, 10000, 12500],
     # synthetic_uniform: Never reaches 0.99 (max 0.96 at 80000)
     'synthetic_uniform': [10000, 20000, 40000, 80000, 160000, 320000],
     # sift1m: 0.99 at search_k~40000
     'sift1m': [10000, 20000, 40000, 80000, 120000, 200000],
-    'glove': [80000, 120000, 200000],
+    'glove': [300000, 400000, 500000],
 }
 
 
